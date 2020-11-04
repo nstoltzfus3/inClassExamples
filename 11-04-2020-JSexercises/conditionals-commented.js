175faa465e5abaa85163a30e344566dcecc7cf66
nstoltzfus <nstoltzfus3@gatech.edu>
2020-11-04 02:15:10 -0800

    conditionals added and comments removed for clean version

---
+++ 11-04-2020-JSexercises/conditionals-commented.js
@@ -0,0 +1,17 @@
+//conditional practice
+
+// a basic if statement, it runs the code inside the
+// {} when the (condition) is true.
+let iAmTrue = true;
+if (iAmTrue) {
+        console.log("True Test")
+}
+
+// a layered if statement
+if (false) {
+        console.log("This will never run.")
+} else { 
+        // we run what is in the  "else" block because the main
+        // 'if' statement condition evaluated to false.
+        console.log("This will always run")
+}
