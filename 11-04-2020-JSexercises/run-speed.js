4a23fde0d0174a2018fa6293a3dc71ca9f9105f5
nstoltzfus <nstoltzfus3@gatech.edu>
2020-11-04 02:22:54 -0800

    Answer added from homework.

---
+++ 11-04-2020-JSexercises/run-speed.js
@@ -0,0 +1,15 @@
+let startTimes = [5,7,0,10,4];
+speeds = [1.4, 1.5, .5, 2, 1.2];
+distances = [0,0,0,0,0];
+
+seconds = 40;
+
+for (let i = 0; i < seconds; i++) {
+    for (let j = 0; j < startTimes.length; j++) {
+        if (i > startTimes[j]) { // implementation based, can be >= also.
+            distances[j] += speeds[j];
+        }
+    }
+}
+
+console.log(distances)
